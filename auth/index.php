29152dfb39d8feb8dbf831df963a6b9b4107b5ac
Shivam srivastava <er1shivam@gmail.com>
2016-10-09 04:14:24 +0530

    Add custom css and template

--- auth/index.php
+++ auth/index.php
@@ -5,20 +5,62 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Homepage</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ORRS</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
-<h2>User Authentication System </h2><hr>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 
-<?php if(!isset($_SESSION['username'])): ?>
-<p> You are currently not sign in <a href="login.php">Login</a>Not yet a member?
-<a href="signup.php">Sign up</a>
-</p>
-<?php else: ?>
-<p>
-You are logged in as <?php echo $_SESSION['username']; ?><a href="logout.php">log out </a> </p>
+    <div class="container">
+
+      <div class="flag">
+        <h1>O R R S</h1>
+        <p class="lead">This is a online railway reservation system in PHP .<br>
+         Right now i am working on user interface</p>
+
+
+        <?php if(!isset($_SESSION['username'])): ?>
+        <p> You are currently not sign in <a href="login.php">Login </a>Not yet a member?
+        <a href="signup.php">Sign up</a>
+        </p>
+        <?php else: ?>
+        <p>
+        You are logged in as <?php echo $_SESSION['username']; ?><a href="logout.php">log out </a> </p>
 <?php endif ?>
 
 
+      </div>
+      
+    </div>
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
