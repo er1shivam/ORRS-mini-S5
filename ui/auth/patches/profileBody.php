627239f214b73c2af88c9728de3458dc7237c3e5
Shivam srivastava <er1shivam@gmail.com>
2016-10-12 18:46:18 +0530

    Add profile update feature in the profile section

--- ui/auth/patches/profileBody.php
+++ ui/auth/patches/profileBody.php
@@ -2,11 +2,16 @@
 include_once('resource/Database.php');
 require_once('resource/utilities.php');
 
-if(isset($_SESSION['id'])){
-    $id = $_SESSION['id'];
-
-
-
+if((isset($_SESSION['id']) || isset($_GET['user_identity'])) && !isset($_POST['updateProfileBtn'])){
+    if(isset($_GET['user_identity'])){
+        $url_encoded_id = $_GET['user_identity'];
+        $decode_id = base64_decode($url_encoded_id);
+        $user_id_array = explode("encodeuserid", $decode_id);
+        $id = $user_id_array[1];
+    }else{
+        $id = $_SESSION['id'];
+    }
+    // to list the profile details on profile page
     $sqlQuery = "SELECT * FROM users WHERE id = :id";
     $statement = $db->prepare($sqlQuery);
     $statement->execute(array(':id' => $id));
@@ -16,7 +21,48 @@ if(isset($_SESSION['id'])){
         $email = $rs['email'];
         $date_joined = strftime("%b %d, %Y", strtotime($rs["datejoined"]));
     }
-
     $encode_id = base64_encode("encodeuserid{$id}");
+}else if(isset($_POST['updateProfileBtn'])){
+    //array to store errors
+    $form_errors = array();
 
-} 
+    $required_fields = array('email', 'username');
+    //check errors and store the merge all errors
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+
+    $fields_to_check_length = array('username' => 4);
+    $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
+    //collect data
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $hidden_id = $_POST['hidden_id'];
+
+    if(empty($form_errors)){
+        try{
+            //update user profile details
+            $sqlUpdate = "UPDATE users SET username =:username, email =:email WHERE id =:id";
+            $statement = $db->prepare($sqlUpdate);
+            $statement->execute(array(':username' => $username, ':email' => $email, ':id' => $hidden_id));
+
+            if($statement->rowCount() == 1){
+                $result = "<script type=\"text/javascript\">
+                swal(\"Updated!\",\"Profile Update Successfully.\",\"success\"); </script>";
+            }else{
+                $result = "<script type=\"text/javascript\">
+                swal(\"Nothing happened\",\"You have not made any changes.\"); </script>";
+            }
+
+        }catch(PDOException $ex){
+            $result = flashMessage("An error occured in :". $ex->getMessage());
+        }
+
+    }
+    else{
+        if(count($form_errors) == 1){
+             $result  = flashMessage("There was one error in the form <br/>");
+        }
+        else{
+        $result  = flashMessage("There were " . count($form_errors). " errors in the form <br />");
+        }
+    }
+}  
